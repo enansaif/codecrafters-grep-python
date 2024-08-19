e7bcd8c8894a2b5a241148b84d5894592be991d2
enansaif <enansaifme33@gmail.com>
2024-08-19 17:56:19 +0600

    test

--- app/main.py
+++ app/main.py
@@ -72,29 +72,6 @@ def match_character_classes(input_line, pattern):
         return False
     return True
 
-def match_character_classes(input_line, pattern):
-    i, j = 0, 0
-    while i < len(pattern) and j < len(input_line):
-        if pattern[i].isalnum() or pattern[i].isspace():
-            if pattern[i] != input_line[j]:
-                return False
-            i += 1
-            j += 1
-        else:
-            if pattern[i + 1] == 'd':
-                if not input_line[j].isdigit():
-                    return False
-            if pattern[i + 1] == 'w':
-                if not input_line[j].isalnum():
-                    return False
-            i += 2
-            j += 1
-
-    if i != len(pattern):
-        return False
-    return True
-
-
 def match_pattern(input_line, pattern):
     if len(pattern) > 0 and pattern[0] == '[':
         return match_character_groups(input_line, pattern[1:-1])
