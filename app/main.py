bbc05ca9ca2ca3aaea4698206186f8205a15c141
enansaif <enansaifme33@gmail.com>
2024-08-22 21:33:09 +0600

    removed alternation

--- app/main.py
+++ app/main.py
@@ -25,22 +25,8 @@ def handle_backslash(input_line, pattern, ii, pi):
     return ii + 1, pi + 2
 
 def handle_literals(input_line, pattern, ii, pi):
-    if pi >= len(pattern):
-        return True
-    if ii >= len(input_line):
-        return False
     if pattern[pi] == '.':
         return ii + 1, pi + 1
-    if pattern[pi] == '(':
-        i = pi
-        while pattern[i] != ')':
-            i += 1
-        words = pattern[pi + 1 : i].split('|')
-        for word in words:
-            if input_line[ii: ii + len(word)] == word:
-                if handle_literals(input_line, pattern, ii + len(word), i + 1):
-                    return True
-        return False
     if pattern[pi] != input_line[ii]:
         return None
     return ii + 1, pi + 1
